17b7c49dab292d855ae64361fd42dd980214a419
monabiesdorf <72738583+monabiesdorf@users.noreply.github.com>
2023-11-06 00:07:09 +0100

    ESA3

--- EA1_IA_Scheibe.js
+++ EA1_IA_Scheibe.js
@@ -1,19 +1,8 @@
-let containerScheibe = document.querySelector('.containerScheibe');
+/* let containerScheibe = document.querySelector('.containerScheibe');
 let moveBy = 10;
 
 window.addEventListener('load', () => {
     containerScheibe.style.position = 'absolute';
-    containerScheibe.style.left = 0;
+    containerScheibe.style.left = 1;
 });
-
-window.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'l':
-            containerScheibe.style.left = parseInt(containerScheibe.style.left) - moveBy + frameElement;
-            break;
-        case 'r':
-            containerScheibe.style.left = parseInt(containerScheibe.style.left) + moveBy + 'px';
-            break;
-
-    }
-});
+ */
